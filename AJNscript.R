e5e8a98f543438527727784f8ca4cde9436f7422
Niemela <Amanda.Niemela@water.ca.gov>
2025-01-29 14:43:07 -0800

    test code

---
+++ AJNscript.R
@@ -0,0 +1,28 @@
+
+# GETTING SET UP ###############################
+
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("readr")
+install.packages("vegan")
+install.packages("readxl")
+
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(readr)
+library(vegan)
+
+# Data #################################
+
+
+morph_observed<-read_excel("X:/Special Studies/Smelt_Morphometrics/data/NDFS2024_smelt_morphometrics.xlsx")
+
+# creating subset ####
+## Subset includes only smelt that were photographed in aquarium ####
+
+Aquarium_Smelt <- subset(morph_observed, !is.na(Photo_fv)) #Note to self: is.na used to check for N/A, ! NEGATES logical condition
+view(Aquarium_Smelt)
+
+nonfv_smelt <- subset(morph_observed, is.na(Photo_fv))
+view(nonfv_smelt)
